4848479db83d112e379ab0ccc4dc9480257b3230
jtalladay <talladay@email.uscupstate.edu>
2022-03-01 10:38:53 -0500

    added "game" program to provide an example for class creation

---
+++ Game.java
@@ -0,0 +1,46 @@
+//Example from class creating new class and objects
+
+// Class defining a die
+class Die {
+  // State of the die; face on top
+  private int face_Value; // this is an instance variable 
+  public static final int max = 6;
+  //Constructor for Die class: no "parameters" no passed in information
+  public Die (){
+    face_Value = 1;
+  }
+  // Constructor for Die class: accepts an integer
+  public Die (int starting_Value){
+    face_Value = starting_Value;
+  }
+
+  public void roll () {
+    face_Value = (int)(Math.random() * 6) + 1;
+  }
+
+  public String toString (){
+    return "Die with value " + face_Value;
+
+  }
+  public void set_Value (int new_Value){
+    if (new_Value >= 1 && new_Value <= max){
+      face_Value = new_Value;
+    }
+  }
+}
+
+class Game {
+  public static void main(String []args){
+    Die d1 = new Die();
+    Die d2 = new Die();
+    Die d3 = new Die();
+    d1.roll();
+    d2.roll();
+    d3.roll();
+    System.out.println(d1);
+    System.out.println(d2);
+    System.out.println(d3);
+
+  }
+
+}
